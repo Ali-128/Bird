09681172bbd9a7a89563a6bd8456b344e8be0b68
ahg <a.h.golshan.nia@gmail.com>
2020-04-23 22:11:47 +0430

    add rum method

--- gameplay.py
+++ gameplay.py
@@ -1,8 +1,14 @@
 import pygame
 import sys
 
-
+def run(width,height):
+	pygame.init()
+	screen=pygame.display.set_mode((width,height))
+	while True:
+		for event in pygame.event.get():
+			if event.type==pygame.QUIT:
+				sys.exit(0)
 def main(args):
-	pass
+	run(500,500)
 if __name__=='__main__':
 	main(sys.argv)
