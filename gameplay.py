34d8c138efd0a0f47a80715728d648dd6b306d72
ahg <a.h.golshan.nia@gmail.com>
2020-04-23 22:55:39 +0430

    update gameplay

--- gameplay.py
+++ gameplay.py
@@ -1,13 +1,19 @@
 import pygame
 import sys
-
+import objects
 def run(width,height):
 	pygame.init()
 	screen=pygame.display.set_mode((width,height))
+	bird=objects.Bird(screen)
+	stuffs=[]
 	while True:
 		for event in pygame.event.get():
 			if event.type==pygame.QUIT:
 				sys.exit(0)
+		screen.fill((255,255,255))
+		bird.update()
+		bird.show()
+		pygame.display.update()
 def main(args):
 	run(500,500)
 if __name__=='__main__':
