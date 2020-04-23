5ae0c4f761202bf3b3861651fdc9e1801ed93dc6
ahg <a.h.golshan.nia@gmail.com>
2020-04-23 21:52:55 +0430

    add class

--- objects.py
+++ objects.py
@@ -1,1 +1,17 @@
-
+import pygame
+class Bird:
+	def __init__(self,screen):
+		self.screen=screen
+		pass
+	def update(self):
+		pass
+	def show(self):
+		pass
+class Stuff:
+	def __init__(self,screen):
+		self.screen=screen
+		pass
+	def update(self):
+		pass
+	def show(self):
+		pass
