07eba490e6517285aff0191a77156accfe3108f5
Ali-128 <ali_hassani128@yahoo.com>
2020-04-24 22:13:42 +0430

    Update objects.py

--- objects.py
+++ objects.py
@@ -14,13 +14,12 @@ class Bird:
 		pass
 	def gameover(self):
 		text="Game Over"
-		text_font=pygame.font.Font('freesansbold.ttf',50)
+		text_font=pygame.font.Sysfont('freesansbold.ttf',50)
 		textSurface=text_font.render(text,True,(255,255,255))
 		textRect=textSurface.get_rect()
 		textRect=((self.screen.get_width()//2),(self.screen.get_height()//2))
 		self.screen.blit(textSurface,textRect)
 		pygame.display.update()
-
 class Stuff:
 	def __init__(self,screen):
 		self.screen=screen
