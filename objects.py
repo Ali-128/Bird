a652001c102cd30106e0f38dc6875c5d4a4d3f3d
Ali-128 <ali_hassani128@yahoo.com>
2020-04-24 21:52:09 +0430

    add game over method to Bird class

--- objects.py
+++ objects.py
@@ -6,12 +6,20 @@ class Bird:
 		self.bird_y=bird_y
 		self.speed_X=speed_X
 		self.speed_Y=speed_Y
-		self.image=pygame.image.load("redbird-upflap.png")
+		self.image=pygame.image.load("picture/redbird-midflap.png")
 		
 	def update(self):
 		pass
 	def show(self):
 		pass
+	def gameover(self):
+		text="Game Over"
+		text_font=pygame.font.Font('freesansbold',50)
+		textSurface=text_font.render(text,True,(255,255,255))
+		textRect=textSurface.get_rect()
+		textRect=((self.screen.get_width()//2),self.screen.get_height//2)
+		self.screen.blit(textSurface,textRect)
+		pygame.display.update()
 class Stuff:
 	def __init__(self,screen):
 		self.screen=screen
