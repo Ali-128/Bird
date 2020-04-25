5fd530d1922ec37a6d56b9c7a9e5157671ba22db
ahg <a.h.golshan.nia@gmail.com>
2020-04-25 21:02:34 +0430

    adding bal bal

--- gameplay.py
+++ gameplay.py
@@ -6,16 +6,12 @@ def run(width,height):
 	screen=pygame.display.set_mode((width,height))
 	pygame.display.set_caption("Flying Bird")
 	back=pygame.image.load('pictures/sky.png')
-	pipe=pygame.image.load('pictures/pipe-green.png')
-	bird=objects.Bird(screen,100,100,0,0)
+	bird=objects.Bird(screen,100,100,0,0,0.1)
 	while True:
-		'''for event in pygame.event.get():
-			if event.type==pygame.QUIT:
-				sys.exit(0)'''
 		screen.blit(back,(0,0))
-		screen.blit(pipe,(200,0))
-		bird.update()
+		#bird.update()
 		bird.show()
+		bird.update()
 		pygame.display.update()
 def main(args):
 	run(800,600)
--- objects.py
+++ objects.py
@@ -1,40 +1,41 @@
 import pygame
 class Bird:
-	def __init__(self,screen,bird_x,bird_y,speed_X,speed_Y):
+	def __init__(self,screen,x,y,speed_x,speed_y,g):
 		self.screen=screen
-		self.bird_x=bird_x
-		self.bird_y=bird_y
-		self.speed_X=speed_X
-		self.speed_Y=speed_Y
-		self.g=0.1
-		self.go=False
-		self.image=pygame.image.load("pictures/redbird-midflap.png")
+		self.x=x
+		self.y=y
+		self.speed_x=speed_x
+		self.speed_y=speed_y
+		self.g=g
+		self.images=[pygame.image.load("pictures/redbird-downflap.png"),pygame.image.load("pictures/redbird-midflap.png"),pygame.image.load("pictures/redbird-upflap.png")]
+		self.counter=0
 	def gameover(self):
 			text="Game Over"
 			text_font=pygame.font.SysFont('freesansbold.ttf',50)
 			textSurface=text_font.render(text,True,(255,255,255))
 			textRect=textSurface.get_rect()
-			textRect=((self.screen.get_width()//2),(self.screen.get_height()//2))
+			textRect=((self.screen.get_width()//2-85),(self.screen.get_height()//2-30))
 			self.screen.blit(textSurface,textRect)
 			pygame.display.update()
 	def update(self):
 		for event in pygame.event.get():
 			if event.type==pygame.KEYDOWN:
 				if event.key==pygame.K_SPACE:
-						self.speed_Y-=self.g*40
-		if self.speed_Y > 0:
-			self.speed_Y+=self.g
+						self.speed_y-=self.g*40
+		if self.speed_y > 0:
+			self.speed_y+=self.g
 		else:
-			self.speed_Y+=1.5*self.g
-		self.bird_y+=self.speed_Y
-		if self.bird_y < 0 or self.bird_y > self.screen.get_height():
+			self.speed_y+=1.7*self.g
+		self.y+=self.speed_y
+		if self.y < 0 or self.y > self.screen.get_height():
 			self.gameover()
-			self.speed_Y=0
+			self.speed_y=0
 			self.g=0
+		self.counter+=1
+		if self.counter == 3:
+			self.counter = 0
 	def show(self):
-		self.screen.blit(self.image,(self.bird_x,self.bird_y))
-
-
+		self.screen.blit(self.images[self.counter % 3],(self.x,self.y))
 class Stuff:
 	def __init__(self,screen):
 		self.screen=screen
